76b9fc0d285c95ef5771e239c3e56008f00c0b12
LucRome <64947931+LucRome@users.noreply.github.com>
2020-11-04 21:20:00 +0100

    implement all functions in Node

--- Aufgaben/ScannerAndParser/Node.cpp
+++ Aufgaben/ScannerAndParser/Node.cpp
@@ -1,1 +1,37 @@
 #include "Node.h"
+#include "Event.h"
+#include "Task.h"
+#include "State.h"
+#include "Transition.h"
+#include "Event_Declaration.h"
+#include "State_Transitions.h"
+
+void Node::push(Event& e)
+{
+	event_list.push_back(std::make_shared<Event>(e));
+}
+
+void Node::push(Task& t)
+{
+	task_list.push_back(std::make_shared<Task>(t));
+}
+
+void Node::push(State& s)
+{
+	state_list.push_back(std::make_shared<State>(s));
+}
+
+void Node::push(Transition& t)
+{
+	transition_list.push_back(std::make_shared<Transition>(t));
+}
+
+void Node::push(Event_Declaration& ed)
+{
+	statement_list.push_back(std::make_shared<Event_Declaration>(ed));
+}
+
+void Node::push(State_Transitions& st)
+{
+	statement_list.push_back(std::make_shared<State_Transitions>(st));
+}
--- Aufgaben/ScannerAndParser/Node.h
+++ Aufgaben/ScannerAndParser/Node.h
@@ -1,15 +1,31 @@
 #pragma once
+#include <vector>
+#include <memory>
 
 class Event;
 class Task;
 class State;
 class Transition;
+class Event_Declaration;
+class State_Transitions;
+class Dec_Type;
+class Statement;
+
 class Node
 {
-public:
-	virtual void push(Event& e) = 0;
-	virtual void push(Task& t) = 0;
-	virtual void push(State& s) = 0;
-	virtual void push(Transition& t) = 0;
+protected:
+	virtual void push(Event& e);
+	virtual void push(Task& t);
+	virtual void push(State& s);
+	virtual void push(Transition& t);
+	virtual void push(Event_Declaration& ed);
+	virtual void push(State_Transitions& st);
+
+	/*potential children, delete unneccessary*/
+	std::vector<std::shared_ptr<Statement>> statement_list;
+	std::vector<std::shared_ptr<Dec_Type>> event_list;
+	std::vector<std::shared_ptr<Dec_Type>> task_list;
+	std::vector<std::shared_ptr<Dec_Type>> state_list;
+	std::vector<std::shared_ptr<Dec_Type>> transition_list;
 };
 
